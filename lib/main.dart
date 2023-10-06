c8f63f3560a5becae144cfcdd9bc3b4c1b117343
Yves Atsuchi Takaoka <yvesat@gmail.com>
2023-10-06 19:51:21 -0300

    Criando Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:onfly/view/home.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+  runApp(
+    ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: FlexThemeData.light(scheme: FlexScheme.brandBlue),
+        darkTheme: FlexThemeData.dark(scheme: FlexScheme.brandBlue),
+        themeMode: ThemeMode.system,
+        home: const HomePage(),
       ),
-    );
-  }
+    ),
+  );
 }
