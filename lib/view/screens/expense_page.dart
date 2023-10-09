f3a9b142a9be791e83251aed791eef63360edc77
Yves Atsuchi Takaoka <yvesat@gmail.com>
2023-10-09 11:04:32 -0300

    Melhorias de UI na ExpensePage
    Adicionado circular progress indicator;
    Esconde teclado ao clicar em "Salvar";

--- lib/view/screens/expense_page.dart
+++ lib/view/screens/expense_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:onfly/controller/expenses_controller.dart';
 import 'package:onfly/view/widgets/button.dart';
+
+import '../widgets/progress.dart';
 
 class ExpensePage extends ConsumerStatefulWidget {
   final String? expenseId;
@@ -21,6 +24,7 @@ class _CreateExpensePageState extends ConsumerState<ExpensePage> {
 
   @override
   Widget build(BuildContext context) {
+    final expenseControllerState = ref.watch(expenseControllerProvider);
     final expenseController = ref.read(expenseControllerProvider.notifier);
     if (widget.expenseId != null) {
       final expense = expenseController.getExpense(ref, widget.expenseId!);
@@ -29,94 +33,103 @@ class _CreateExpensePageState extends ConsumerState<ExpensePage> {
       expenseDate = expense.date;
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Card(
-                child: TextFormField(
-                  controller: _titleController,
-                  decoration: const InputDecoration(labelText: 'Descrição'),
-                  validator: (value) {
-                    if (value != null && value.isEmpty) return 'Favor inserir descrição';
-                    return null;
-                  },
-                ),
-              ),
-              InkWell(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                "Data da Despesa: ",
-                                style: TextStyle(fontSize: 12),
-                              ),
-                              const SizedBox(height: 8),
-                              Text(
-                                DateFormat('dd/MM/yyyy').format(expenseDate),
-                                style: const TextStyle(fontSize: 14),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const Icon(Icons.calendar_today),
-                      ],
+    final size = MediaQuery.sizeOf(context);
+
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Card(
+                    child: TextFormField(
+                      controller: _titleController,
+                      textCapitalization: TextCapitalization.words,
+                      decoration: const InputDecoration(labelText: 'Descrição'),
+                      validator: (value) {
+                        if (value != null && value.isEmpty) return 'Favor inserir descrição';
+                        return null;
+                      },
                     ),
                   ),
-                ),
-                onTap: () async {
-                  final newDate = await expenseController.setDate(context, ref, expenseDate);
-                  if (newDate != null) {
-                    setState(() {
-                      expenseDate = newDate;
-                    });
-                  }
-                },
-              ),
-              Card(
-                child: TextFormField(
-                  controller: _valueController,
-                  decoration: const InputDecoration(labelText: 'Valor'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value != null && value.isEmpty) return 'Favor inserir valor';
-                    return null;
-                  },
-                ),
-              ),
-              const Spacer(),
-              Center(
-                child: Button(
-                  label: "Salvar",
-                  onTap: () async {
-                    if (_formKey.currentState!.validate()) {
-                      if (widget.expenseId != null) {
-                        await expenseController.updateExpense(ref, widget.expenseId, _titleController.text, _valueController.text, expenseDate);
-                      } else {
-                        await expenseController.createExpense(ref, _titleController.text, _valueController.text, expenseDate);
+                  InkWell(
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    "Data da Despesa: ",
+                                    style: TextStyle(fontSize: 12),
+                                  ),
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    DateFormat('dd/MM/yyyy').format(expenseDate),
+                                    style: const TextStyle(fontSize: 14),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const Icon(Icons.calendar_today),
+                          ],
+                        ),
+                      ),
+                    ),
+                    onTap: () async {
+                      final newDate = await expenseController.setDate(context, ref, expenseDate);
+                      if (newDate != null) {
+                        setState(() {
+                          expenseDate = newDate;
+                        });
                       }
-                      Navigator.pop(context);
-                    }
-                  },
-                ),
+                    },
+                  ),
+                  Card(
+                    child: TextFormField(
+                      controller: _valueController,
+                      decoration: const InputDecoration(labelText: 'Valor'),
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value != null && value.isEmpty) return 'Favor inserir valor';
+                        return null;
+                      },
+                    ),
+                  ),
+                  const Spacer(),
+                  Center(
+                    child: Button(
+                      label: "Salvar",
+                      onTap: () async {
+                        SystemChannels.textInput.invokeMethod('TextInput.hide');
+                        if (_formKey.currentState!.validate()) {
+                          if (widget.expenseId != null) {
+                            await expenseController.updateExpense(ref, widget.expenseId!, _titleController.text, _valueController.text, expenseDate);
+                          } else {
+                            await expenseController.createExpense(context, ref, _titleController.text, _valueController.text, expenseDate);
+                          }
+                          Navigator.pop(context);
+                        }
+                      },
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
-      ),
+        if (expenseControllerState.isLoading) Progress(size, loadingMessage: "Salvando despesa...")
+      ],
     );
   }
 }
