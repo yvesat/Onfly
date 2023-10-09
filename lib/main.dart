cb44d5576c22fad1d5d7ed5261115327e059cc2f
Yves Atsuchi Takaoka <yvesat@gmail.com>
2023-10-09 19:35:15 -0300

    Removendo solicitação de Token do main
    O método será chamado somente ao tentar comunicar com a API

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
-import 'package:onfly/controller/token_controller.dart';
 import 'package:onfly/view/screens/home_page.dart';
 
 Future<void> main() async {
@@ -10,9 +9,6 @@ Future<void> main() async {
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-
-  final TokenController tokenController = TokenController();
-  await tokenController.getTokenAPI();
 
   runApp(
     ProviderScope(
